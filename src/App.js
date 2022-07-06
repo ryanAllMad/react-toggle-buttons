3c6fa0d9fb548d6abf5eddda219edf98e52640c0
Ryan <46852695+ryanAllMad@users.noreply.github.com>
2022-07-06 16:25:08 -0700

    creating and updating feature branch test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faForward } from '@fortawesome/free-solid-svg-icons';
+import { faBackward } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({label, fontIcon}) => {
+    const [ariaPressed, setAriaPressed] = useState(false); 
+    const [faIcon, setFaIcon]           = useState(fontIcon);
+    const buttonRef = useRef(null);
+
+        //enter button events
+const onButtonClick = () => {
+    if (ariaPressed === false){
+        setAriaPressed(true); 
+        setFaIcon(<FontAwesomeIcon icon={faThumbsUp} />);
+        setTimeout(() => {
+            setAriaPressed(false);
+            setFaIcon(fontIcon);
+        }, 1555);
+ console.log("button clicked");
+    } else {
+        setAriaPressed(false);
+        setFaIcon(fontIcon);
+    }
+}
+    return <button className={label} ref={buttonRef} onClick={onButtonClick} type="button" aria-pressed={ariaPressed}>{faIcon}{label}{faIcon}</button>;
+}
+
+const App = () => {
+    return (
+        <div className="wrapper">
+        <div className=" button-section container">
+            <Button fontIcon={<FontAwesomeIcon icon={faBackward} />} label="previous" />
+            <Button fontIcon={<FontAwesomeIcon icon={faForward} />} label="next" />
+            </div>
+        <div className="container two">
+            <Button fontIcon={<FontAwesomeIcon icon={faPaperPlane} />} label="send" />
+        </div>
+        </div>
+            );
 }
 
 export default App;
